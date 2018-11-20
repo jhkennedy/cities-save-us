27a8204c9dd5baee230197d5d93fef4de985cb58
Joseph H Kennedy <kennedyjh@ornl.gov>
2018-11-20 14:00:04 -0500

    Update CMIP5 preprocessing scripts for new data grid

--- data/CMIP5/fix_CMIP5_emissions_dataset.sh
+++ data/CMIP5/fix_CMIP5_emissions_dataset.sh
@@ -16,8 +16,3 @@ ncatted -O -a missing_value,AREA,o,d,-1.e34 CMIP5_gridcar_CO2_emissions_fossil_f
 ncatted -O -a missing_value,FF,o,d,-1.e34 CMIP5_gridcar_CO2_emissions_fossil_fuel_Andres_1901-2007_monthly_SC_mask11.nc
 ncatted -O -a _FillValue,FF,o,d,-1.e34 CMIP5_gridcar_CO2_emissions_fossil_fuel_Andres_1901-2007_monthly_SC_mask11.nc
 ncatted -O -a _FillValue,AREA,o,d,-1.e34 CMIP5_gridcar_CO2_emissions_fossil_fuel_Andres_1901-2007_monthly_SC_mask11.nc
-
-# ncatted -O -a missing_value,AREA,o,d,-1.e34 CMIP5_gridcar_CO2_emissions_fossil_fuel_Andres_1751-2007_monthly_SC_mask11.nc
-# ncatted -O -a missing_value,FF,o,d,-1.e34 CMIP5_gridcar_CO2_emissions_fossil_fuel_Andres_1751-2007_monthly_SC_mask11.nc
-# ncatted -O -a _FillValue,FF,o,d,-1.e34 CMIP5_gridcar_CO2_emissions_fossil_fuel_Andres_1751-2007_monthly_SC_mask11.nc
-# ncatted -O -a _FillValue,AREA,o,d,-1.e34 CMIP5_gridcar_CO2_emissions_fossil_fuel_Andres_1751-2007_monthly_SC_mask11.nc
